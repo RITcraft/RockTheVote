6fffd63f3b82f265e7a7ea6a3afdf434f4ab8a5f
Justin W. Flory <git@jwf.io>
2016-05-09 19:56:59 -0400

    Add Maven project barebones for the plugin

---
+++ src/main/java/net/ritcraft/RockTheVote/RockTheVote.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 RITcraft
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package net.ritcraft.RockTheVote;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * RockTheVote.java
+ *
+ * This plugin allows users to trigger certain actions on the server based on the popular vote.
+ *
+ * @author Justin W. Flory
+ * @version 2016.05.09.v1
+ */
+public class RockTheVote extends JavaPlugin {
+
+}

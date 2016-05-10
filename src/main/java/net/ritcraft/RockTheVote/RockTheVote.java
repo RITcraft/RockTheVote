dbc60c0d59da63022d1cd79cdee12126404a2d97
BenWoodworth <ben@benwoodworth.net>
2016-05-09 20:27:13 -0400

    Added commands/perms to plugin.yml Added onEnable() and onDisable() methods

--- src/main/java/net/ritcraft/RockTheVote/RockTheVote.java
+++ src/main/java/net/ritcraft/RockTheVote/RockTheVote.java
@@ -20,4 +20,13 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class RockTheVote extends JavaPlugin {
 
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 }
